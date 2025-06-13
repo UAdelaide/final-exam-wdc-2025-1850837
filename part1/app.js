26f70afc7bdfb360063717bd4d1977da0e13d0dc
1850837 <sevensixthree763@gmail.com>
2025-06-13 10:41:55 +0000

    [automatic commit] 2025-06-13 10:41:54 UTC 193.115.102.101

--- part1/app.js
+++ part1/app.js
@@ -1,6 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
+const path = require('node:path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
