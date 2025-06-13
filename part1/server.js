498f392572a5baa3a1cc89e1c5e59008c782360f
1850837 <sevensixthree763@gmail.com>
2025-06-13 08:03:32 +0000

    [automatic commit] 2025-06-13 08:03:31 UTC 193.115.102.101

--- part1/server.js
+++ part1/server.js
@@ -54,7 +54,8 @@ router.get('/api/dogs', async(req, res) => {
 
 router.get('/api/walkrequests/open', async(req, res) => {
     try {
-        
+        const [rows] = await connection.execute(`
+            `)
     } catch (err) {
         console.error('Error getting walkrequests:', err.message);
         res.status(500).json({ error: 'Failed to retrieve walkrequests' });
