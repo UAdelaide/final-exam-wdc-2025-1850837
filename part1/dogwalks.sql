74210c860064b453b3fc5968acd5e8642e2d0ff7
1850837 <sevensixthree763@gmail.com>
2025-06-13 05:15:08 +0000

    [automatic commit] 2025-06-13 05:15:07 UTC 193.115.102.101

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -57,8 +57,8 @@ CREATE TABLE WalkRatings (
 INSERT INTO Users
     (username,email,password_hash,role)
 VALUES
-    (alice123,alice@example.com,hashed123,"owner"),
-    (bobwalker,bob@example.com,hashed456,"walker"),
-    (carol123,carol@example.com,hashed789,"owner"),
-    (deliaw,delia@example.com,password123,"walker"),
+    (alice123,alice@example.com,hashed123,'owner'),
+    (bobwalker,bob@example.com,hashed456,'walker'),
+    (carol123,carol@example.com,hashed789,'owner'),
+    (deliaw,delia@example.com,password123,'walker'),
     (elliott1,elliott@example.com,password456,"owner");
