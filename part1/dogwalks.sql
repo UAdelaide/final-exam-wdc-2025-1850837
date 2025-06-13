a48719bb0626ef9c760ccea2b2ecc809e5bff724
1850837 <sevensixthree763@gmail.com>
2025-06-13 05:30:40 +0000

    [automatic commit] 2025-06-13 05:30:39 UTC 193.115.102.101

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -69,4 +69,5 @@ VALUES
     (SELECT user_id FROM Users WHERE username='alice123','Max','medium'),
     (SELECT user_id FROM Users WHERE username='carol123','Bella','small'),
     (SELECT user_id FROM Users WHERE username='elliott1','Wallace','large'),
+    (SELECT user_id FROM Users WHERE username='elliott1','Harry','large'),
     (SELECT user_id FROM Users WHERE username='elliott1','')
