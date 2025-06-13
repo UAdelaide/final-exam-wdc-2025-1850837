6f74a46d93d5a1c6a534bfd4f3065ea19419dc0a
1850837 <sevensixthree763@gmail.com>
2025-06-13 07:42:24 +0000

    [automatic commit] 2025-06-13 07:42:24 UTC 193.115.102.101

--- part1/server.js
+++ part1/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql12/promise');
 const fs = require('fs');
 const path = require('path');
+const dogwalksRouter = require('./routes/index');
 
 const app = express();
 const port = 3000;
