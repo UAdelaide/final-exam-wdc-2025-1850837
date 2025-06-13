bae840b78de4d62da8f08f01cb3843daf73f7de2
1850837 <sevensixthree763@gmail.com>
2025-06-13 07:07:15 +0000

    [automatic commit] 2025-06-13 07:07:14 UTC 193.115.102.101

--- part1/app.js
+++ part1/app.js
@@ -10,6 +10,9 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 var mysql = require('mysql');
+var con = mysql.createConnection({
+  
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
