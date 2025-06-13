b33a890434b95ded54e2b15e995b55f09f8837be
1850837 <sevensixthree763@gmail.com>
2025-06-13 07:08:21 +0000

    [automatic commit] 2025-06-13 07:08:20 UTC 193.115.102.101

--- part1/app.js
+++ part1/app.js
@@ -12,7 +12,8 @@ var app = express();
 var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
-  user: "host"
+  user: "root",
+  password: ""
 })
 
 // view engine setup
