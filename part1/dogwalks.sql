7c0c0b3a4b3413faed7a5f06269c7de0cec2d1e0
1850837 <sevensixthree763@gmail.com>
2025-06-13 05:29:13 +0000

    [automatic commit] 2025-06-13 05:29:12 UTC 193.115.102.101

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -66,4 +66,5 @@ VALUES
 INSERT INTO Dogs
     (owner_id, name, size)
 VALUES
-    (SELECT user_id FROM Users WHERE username='alice123','Max','medium')
+    (SELECT user_id FROM Users WHERE username='alice123','Max','medium'),
+    
